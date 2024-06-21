f2ada488175231fb60353ae5bf7cec4a4874379c
thanhnm <duc.la@adamo.software>
2024-06-21 10:35:20 +0700

    dsds

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Integer a[] = {1,3,4,5,6};
        ArrayList<Integer> listInteger = new ArrayList<>(Arrays.asList(a));
+        System.out.println("dev branch");
        listInteger.forEach(x -> {
            if (x % 2 == 0) {
                return;
