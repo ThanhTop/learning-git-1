13eaf1c84cb341a235acaf9436c140fbd19f4fff
thanhnm <duc.la@adamo.software>
2024-06-21 10:19:57 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a[] = {1,3,4,5,6};
+       ArrayList<Integer> listInteger = new ArrayList<>(Arrays.asList(a));
+       listInteger.forEach(x -> {
+           if (x % 2 == 0) {
+               return;
+           }
+           System.out.println(x);
+       });
+        System.out.println("dev branch");
+
+
+
+
+    }
+
+}
